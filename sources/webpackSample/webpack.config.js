c231ee38795892dfd5ed9dc9e09e76ea8d43be7b
DESKTOP-9MOB\wedo <wh_kendny@163.com>
2018-02-28 15:27:02 +0800

    add

--- sources/webpackSample/webpack.config.js
+++ sources/webpackSample/webpack.config.js
@@ -2,10 +2,13 @@
   注意: “__dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录。
 
 */
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: __dirname + "/app/main.js",
   output: {
-    path: __dirname + "/public",
+    path: __dirname + "/build",
     filename: "bundle.js"
   },
   devtool: 'eval-source-map',
@@ -47,5 +50,12 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins:[
+    new webpack.BannerPlugin('版权所有，翻版必究'),
+    new HtmlWebpackPlugin({
+      template: __dirname + "/app/index.tmpl.html" //new 一个这个插件的实例，并传入相关的参数
+    }),
+    new webpack.HotModuleReplacementPlugin() //热加载模块
+  ]
 }
